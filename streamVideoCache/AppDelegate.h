5282b829eb61c61ea91d75ee60d00a703a1d6241
Eugene Golub <golub.e@epsysoft.com.ua>
2015-11-04 14:04:17 +0200

    Initial Commit

---
+++ streamVideoCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  streamVideoCache
+//
+//  Created by rash on 11/4/15.
+//  Copyright © 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
